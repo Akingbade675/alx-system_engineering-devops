b904489ac42c9922536081017b5b937d52a7ff04
Akingbade675 <akingbadebasit26@gmail.com>
2023-01-20 16:40:06 +0000

    fixed problems

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,12 @@
 # setting up client SSH config file
-include stdlib
-
 file_line { 'Turn off passwd auth':
     ensure  => 'present',
     path    => '/etc/ssh/ssh_config',
     line    => '    PasswordAuthentication no',
-    replace => true,
 }
 
 file_line { 'Turn off passwd auth':
     ensure  => 'present',
     path    => '/etc/ssh/ssh_config',
     line    => '   IdentifyFile ~/.ssh/school',
-    replace => true,
 }
